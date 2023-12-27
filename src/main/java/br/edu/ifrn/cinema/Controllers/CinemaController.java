c6b04f21ff21dfce75de243d44c39422fa0c060d
Lucas Gabriel <lucasgabriel181105@gmail.com>
2023-12-27 13:47:47 -0300

    ingresso, index, controller

--- src/main/java/br/edu/ifrn/cinema/Controllers/CinemaController.java
+++ src/main/java/br/edu/ifrn/cinema/Controllers/CinemaController.java
@@ -1,54 +1,60 @@
 package br.edu.ifrn.cinema.Controllers;
 
+import br.edu.ifrn.cinema.models.Ingresso;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+@Controller
 public class CinemaController {
+    
     @GetMapping("/")
-    public String index(Model model){
-        List<Ingresso> ingressos = Ingresso.listar(jdbc); 
-        model.addAttribute("ingresso", ingresso);
+    public String index(Model model) {
+        List<Ingresso> ingressos =
+                Ingresso.listar(jdbc);
+        model.addAttribute("ingressos",
+                ingressos);
         return "index";
     }
     
     @Autowired JdbcTemplate jdbc;
     
     @PostMapping("/adicionar")
-    public String adicionar(String nome, int quantidade, float preco){
-        Ingresso i = new Ingresso(nome, quantidade, preco);
+    public String adicionar(String nome, float preco) {
+        Ingresso i = new Ingresso(nome, preco);
         i.salvar(jdbc);
         return "redirect:/";
     }
     
     @PostMapping("/atualizar")
-    public String atualizar(int id, String nome, int quantidade, float preco){
-        Produto p = Produto.buscar(id, jdbc);
-        p.setNome(nome);
-        p.setQuantidade(quantidade);
-        p.setPreco(preco);
-        p.salvar(jdbc);
+    public String atualizar(int id, 
+            String nome, float preco) {
+        Ingresso i = Ingresso.buscar(id, jdbc);
+        i.setNome(nome);
+        i.setPreco(preco);
+        i.salvar(jdbc);
         return "redirect:/";
-        
     }
     
     @GetMapping("/excluir")
-    public String excluir(int id){
-        Produto.remover(id, jdbc);
+    public String excluir(int id) {
+        Ingresso.remover(id, jdbc);
         return "redirect:/";
-        
     }
     
     @GetMapping("/editar")
-    public String editar(int id, Model model){
-        List<Produto> produtos = Produto.listar(jdbc); 
-        Produto p = Produto.buscar(id, jdbc);
-        model.addAttribute("produtos", produtos);
-        model.addAttribute("p", p);
+    public String editar(int id, Model model) {
+        List<Ingresso> ingressos =
+                Ingresso.listar(jdbc);
+        Ingresso i = Ingresso.buscar(id, jdbc);
+        model.addAttribute("ingressos",
+                ingressos);
+        model.addAttribute("i", i);
         return "index";
-        
     }
+    
 }
--- src/main/java/br/edu/ifrn/cinema/models/Ingresso.java
+++ src/main/java/br/edu/ifrn/cinema/models/Ingresso.java
@@ -1,5 +1,101 @@
 package br.edu.ifrn.cinema.models;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicReference;
+import org.springframework.jdbc.core.JdbcTemplate;
+
 public class Ingresso {
+    
+    private int id;
+    private String nome;
+    private float preco;
+
+    public Ingresso(String nome, float preco) {
+        this.nome = nome;
+        this.preco = preco;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public float getPreco() {
+        return preco;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setPreco(float preco) {
+        this.preco = preco;
+    }
+    
+    public void salvar(JdbcTemplate jdbc) {
+        if (id > 0) {
+            jdbc.update(
+            "UPDATE ingressos SET nome = ?, "
+            + "preco = ? "
+            + "WHERE id = ?;", (ps) -> {
+                ps.setString(1, nome);
+                ps.setFloat(2, preco);
+                ps.setInt(3, id);
+            });
+        } else {
+            jdbc.update(
+            "INSERT INTO ingressos (nome, preco) " +
+            "VALUES (?, ?);", (ps) -> {
+                ps.setString(1, nome);
+                ps.setFloat(2, preco);
+            });
+        }
+    }
+    
+    public static List<Ingresso> listar(JdbcTemplate
+            jdbc) {
+        List<Ingresso> ingressos =
+                new ArrayList<>();
+        jdbc.query("SELECT * FROM ingressos;",
+        (rs) -> {
+            do {
+                Ingresso i = new Ingresso(
+                        rs.getString("nome"),
+                        rs.getFloat("preco"));
+                i.id = rs.getInt("id");
+                ingressos.add(i);
+            } while(rs.next());
+        });
+        return ingressos;
+    }
+    
+    public static void remover(int id,
+            JdbcTemplate jdbc) {
+        jdbc.update("DELETE FROM ingressos "
+        + "WHERE id = ?", (ps) -> {
+            ps.setInt(1, id);
+        });
+    }
+    
+    public static Ingresso buscar(int id,
+            JdbcTemplate jdbc) {
+        AtomicReference<Ingresso> ingresso
+                = new AtomicReference<>();
+        jdbc.query("SELECT * FROM ingressos "
+        + "WHERE id = ?;", (ps) -> {
+            ps.setInt(1, id);
+        }, (rs) -> {
+            Ingresso i = new Ingresso(
+                    rs.getString("nome"),
+                    rs.getFloat("preco"));
+            i.id = rs.getInt("id");
+            ingresso.set(i);
+        });
+        return ingresso.get();
+    }
     
 }
